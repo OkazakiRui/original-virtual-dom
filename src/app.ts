c57a63ae2086cf460a8331f7403c76fe4a8cc063
RuiOkazaki <okazaki200226@gmail.com>
2022-05-09 01:49:44 +0900

    update: main loop を作成し、Hello world! を出力した

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
+import createElement from './func/createElement';
 import h from './func/createVNode';
 
-console.log(h('div', { class: 'sample-dom' }, ['Hello World']));
+const main = () => {
+  const appElement = document.getElementById('app');
+  if (!appElement) throw new Error('#app が存在しません');
+  appElement.appendChild(
+    createElement(h('div', { id: 'new-dom' }, ['Hello World!']))
+  );
+};
+main();
