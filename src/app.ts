1c361e7617adc1ae6a66c3a702e9e4c45e6e2988
RuiOkazaki <okazaki200226@gmail.com>
2022-05-09 13:59:14 +0900

    feat: 差分を検知するdiff関数を実装した

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import createElement from './func/createElement';
 import h from './func/createVNode';
+import diff from './func/diff';
 
 const main = () => {
   const appElement = document.getElementById('app');
   if (!appElement) throw new Error('#app が存在しません');
-  appElement.appendChild(
-    createElement(h('div', { id: 'new-dom' }, ['Hello World!']))
-  );
+
+  const oldVNode = h('div', { id: 'new-dom', class: 'test' }, ['Hello World!']);
+  const newVNode = h('div', { id: 'new-dom', class: 'test' }, ['Hello Diff!']);
+
+  const el = createElement(oldVNode);
+  appElement.appendChild(el);
+
+  diff(el, oldVNode, newVNode);
 };
 main();
---
+++ src/func/diff.ts
@@ -0,0 +1,45 @@
+import { VirtualNode } from '../types/VirtualNode';
+import createElement from './createElement';
+
+/**
+ * 差分をチェックし、変更があれば更新する関数
+ * @date 2022-05-09
+ * @param { HTMLElement | ChildNode } el
+ * @param { VirtualNode | string } oldVNode
+ * @param { VirtualNode | string } newVNode
+ */
+const diff = (
+  el: HTMLElement | ChildNode,
+  oldVNode: VirtualNode | string,
+  newVNode: VirtualNode | string
+) => {
+  // 差分を見る最新の vnode が存在しない場合は el を削除する
+  if (!newVNode) return el.remove();
+
+  // 古い vnode が存在しない場合は全てが差分になるので newVNode の要素を作成する
+  if (!oldVNode) return el.appendChild(createElement(newVNode));
+
+  const replace = () => el.replaceWith(createElement(newVNode));
+
+  // どちらかが TextNode の場合は判定をせずに要素を置き換える
+  if (typeof oldVNode === 'string' || typeof newVNode === 'string') {
+    if (oldVNode !== newVNode) return replace();
+  } else {
+    // htmlの要素名が違う場合
+    if (oldVNode.htmlTagName !== newVNode.htmlTagName) return replace();
+
+    // props(属性)が違う場合
+    // entriesでオブジェクトを配列に変換し、sortをすることで順序性を保証することにより差分をチェックする
+    // '[["id","idTest"],["class","classTest"]]'
+    const oldProps = JSON.stringify(Object.entries(oldVNode.props).sort());
+    const newProps = JSON.stringify(Object.entries(newVNode.props).sort());
+    if (oldProps !== newProps) return replace();
+
+    // childrenが違う場合
+    [...el.childNodes].forEach((child, index) => {
+      diff(child, oldVNode.children[index], newVNode.children[index]);
+    });
+  }
+};
+
+export default diff;
