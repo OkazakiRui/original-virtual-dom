b7b23f731cb349a03b37fcdbf0f9f1c8ea7364ea
RuiOkazaki <okazaki200226@gmail.com>
2022-05-09 17:29:47 +0900

    feat: カウンターを実装した

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,42 @@
-import createElement from './func/createElement';
-import diff from './func/diff';
-import mount from './func/mount';
-import globalState from './state';
+import { createState, getState, setState } from './state/index';
+import h from './func/createVNode';
+import './main';
+import globalState, { setVNode } from './state';
 
-/**
- * main loopです。30ms毎に実行されます。
- * @date 2022-05-09
- */
-const main = () => {
-  const { lifecycle, viewElement, oldVNode, newVNode } = globalState;
-  if (lifecycle === 'beforeMount') mount(viewElement);
-  else diff(viewElement, oldVNode, newVNode);
-};
+createState('count', 0);
+setVNode(
+  h('div', { class: 'wrap' }, [
+    h('h3', {}, ['カウント: ', h('span', { id: 'count' }, ['0'])]),
+    h('button', { type: 'button', id: 'increment' }, ['increment']),
+    h('button', { type: 'button', id: 'decrement' }, ['decrement']),
+  ])
+);
 
-main();
-window.setInterval(() => main(), 30);
+const timer: NodeJS.Timer = setInterval(() => {
+  if (globalState.lifecycle === 'beforeMount') return;
+
+  // 処理はこの中に記述する
+  /* ===================================================================== */
+
+  const countElement = document.getElementById('count');
+
+  const increment = () => {
+    setState('count', Number(getState('count')) + 1);
+    if (countElement === null) return;
+    countElement.textContent = String(getState('count'));
+  };
+  const decrement = () => {
+    setState('count', Number(getState('count')) - 1);
+    if (countElement === null) return;
+    countElement.textContent = String(getState('count'));
+  };
+
+  const incrementButton = document.getElementById('increment');
+  const decrementButton = document.getElementById('decrement');
+  incrementButton?.addEventListener('click', () => increment());
+  decrementButton?.addEventListener('click', () => decrement());
+
+  /* ===================================================================== */
+
+  clearInterval(timer);
+}, 30);
