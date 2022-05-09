7ff42cb9e64bf12b410a31dc0a3f3d2ae3b5fdea
RuiOkazaki <okazaki200226@gmail.com>
2022-05-09 15:55:33 +0900

    feat: node関係をstateに移行させた

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,16 @@
 import createElement from './func/createElement';
-import h from './func/createVNode';
 import diff from './func/diff';
 import mount from './func/mount';
 import globalState from './state';
-
-const oldVNode = h('div', { id: 'new-dom', class: 'test' }, ['Hello World!']);
-const newVNode = h('div', { id: 'new-dom', class: 'test' }, ['Hello Diff!']);
-const el = createElement(oldVNode);
 
 /**
  * main loopです。30ms毎に実行されます。
  * @date 2022-05-09
  */
 const main = () => {
-  if (globalState.lifecycle === 'beforeMount') mount(el);
-  else diff(el, oldVNode, newVNode);
+  const { lifecycle, viewElement, oldVNode, newVNode } = globalState;
+  if (lifecycle === 'beforeMount') mount(viewElement);
+  else diff(viewElement, oldVNode, newVNode);
 };
 
 main();
--- src/state/index.ts
+++ src/state/index.ts
@@ -1,7 +1,25 @@
+import createElement from '../func/createElement';
+import h from '../func/createVNode';
+import { VirtualNode } from '../types/VirtualNode';
+
+const defaultElement = h('div', { class: 'wrap' }, [
+  h('h3', {}, ['count: 0']),
+  h('button', { type: 'button' }, ['decrement']),
+  h('button', { type: 'button' }, ['increment']),
+]);
+
 const globalState: {
   lifecycle: 'beforeMount' | 'afterMount';
+  oldVNode: VirtualNode;
+  newVNode: VirtualNode;
+  viewElement: HTMLElement | Text;
+  data: { [key: string]: string };
 } = {
   lifecycle: 'beforeMount',
+  oldVNode: defaultElement,
+  newVNode: defaultElement,
+  viewElement: createElement(defaultElement),
+  data: {},
 };
 
 export default globalState;
