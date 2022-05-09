1452e99c0811f9f2829f528389ad026c29af45b9
RuiOkazaki <okazaki200226@gmail.com>
2022-05-09 14:35:06 +0900

    feat: 30msごとに仮想DOMと描画されているDOMの差分がないかをチェックするようにした

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
 import createElement from './func/createElement';
 import h from './func/createVNode';
 import diff from './func/diff';
+import mount from './func/mount';
+import globalState from './state';
 
+const oldVNode = h('div', { id: 'new-dom', class: 'test' }, ['Hello World!']);
+const newVNode = h('div', { id: 'new-dom', class: 'test' }, ['Hello Diff!']);
+const el = createElement(oldVNode);
+
+/**
+ * main loopです。30ms毎に実行されます。
+ * @date 2022-05-09
+ */
 const main = () => {
-  const appElement = document.getElementById('app');
-  if (!appElement) throw new Error('#app が存在しません');
-
-  const oldVNode = h('div', { id: 'new-dom', class: 'test' }, ['Hello World!']);
-  const newVNode = h('div', { id: 'new-dom', class: 'test' }, ['Hello Diff!']);
-
-  const el = createElement(oldVNode);
-  appElement.appendChild(el);
-
-  diff(el, oldVNode, newVNode);
+  if (globalState.lifecycle === 'beforeMount') mount(el);
+  else diff(el, oldVNode, newVNode);
 };
+
 main();
+window.setInterval(() => main(), 30);
